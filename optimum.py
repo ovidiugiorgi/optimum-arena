7088bd6ad58527e2f9970654f5661918f626d990
Ovidiu Giorgi <ovidiugiorgi@gmail.com>
2016-05-03 19:16:44 +0300

    Modify parser to html.parser Fix issue that created a new output file when the problem's name was invalid

--- optimum.py
+++ optimum.py
@@ -1,7 +1,6 @@
 import requests
 import operator
 from bs4 import BeautifulSoup
-from pip._vendor.distlib.compat import raw_input
 
 d = dict()
 
@@ -9,7 +8,7 @@ def get_best(url):
     url = 'http://www.infoarena.ro' + url
     source_code = requests.get(url)
     plain_text = source_code.text
-    soup = BeautifulSoup(plain_text, "lxml")
+    soup = BeautifulSoup(plain_text, "html.parser")
     name = soup.find('span', {'class': 'username'}).find('a')['href'][35:]
     tests = soup.find_all('td', {'class': 'number'})
     max_ms = -1
@@ -22,7 +21,8 @@ def get_best(url):
         d[name] = (max_ms, url)
     print(max_ms, name, url)
 
-def monitor_spider(pb_name, fw):
+def optimum():
+    pb_name = input("Enter the problem's name: ")
     def_url = 'http://www.infoarena.ro/monitor?task=' + pb_name + '&display_entries=250&first_entry='
     first_entry = 0
     max_entries = -1
@@ -31,7 +31,7 @@ def monitor_spider(pb_name, fw):
         url = def_url + str(first_entry)
         source_code = requests.get(url)
         plain_text = source_code.text
-        soup = BeautifulSoup(plain_text, "lxml")
+        soup = BeautifulSoup(plain_text, "html.parser")
         if max_entries == -1:
             pages = soup.find('span', {'class': 'count'})
             if pages is None:
@@ -49,15 +49,11 @@ def monitor_spider(pb_name, fw):
             get_best(submission.get('href'))
         first_entry += 250
 
+    fw = open(pb_name + '.txt', 'w')
     fw.write('Problem: http://www.infoarena.ro/problema/{}\n'.format(pb_name))
     fw.write('Total submissions: {}\n'.format(max_entries))
     fw.write('Accepted submissions: {}\n'.format(accepted_entries))
     fw.write('Success rate: {0:.2f}%\n\n'.format((accepted_entries / max_entries) * 100))
-
-def optimum():
-    pb_name = input("Enter the problem's name: ")
-    fw = open(pb_name + '.txt', 'w')
-    monitor_spider(pb_name, fw)
     fw.write('Time Leaderboard\n')
     fw.write('-' * 16 + '\n')
     count = 0
